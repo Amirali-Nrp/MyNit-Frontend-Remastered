faa4a0f0ec59c26041cf63216361abe8a9645f41
ErfanZmp <erfanzamirpour@gmail.com>
2025-08-21 04:22:27 +0000

    next auth untrusted fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,10 @@
-export { GET, POST } from "@/lib/auth";
+import NextAuth from "next-auth"
+// import your providers...
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    // ...your providers
+  ],
+  trustHost: true, // <-- important behind proxies / PM2
+})
+export const { GET, POST } = handlers;
